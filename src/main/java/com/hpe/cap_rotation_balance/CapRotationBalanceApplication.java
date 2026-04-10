364d9962d6c29ccc8a29ea6a957ba0cfdda0f85a
ItanRamirezMiramontes <ItanRamirezMiramontesWork@gmail.com>
2026-04-10 08:42:15 -0600

    Estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/hpe/cap_rotation_balance/CapRotationBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.hpe.cap_rotation_balance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapRotationBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapRotationBalanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hpe/cap_rotation_balance/CapRotationBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpe.cap_rotation_balance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapRotationBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
